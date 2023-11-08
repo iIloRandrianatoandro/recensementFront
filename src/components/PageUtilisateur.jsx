c29ffa54ae50c0c8cba6d1340ab75022ebd60fab
unknown <iloorandrianatoandro.com>
2023-11-08 17:51:43 +0300

    page utilisateur et recenser materiel bien commente reste css et datagrid en francais

--- src/components/PageUtilisateur.jsx
+++ src/components/PageUtilisateur.jsx
@@ -9,105 +9,113 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function App() {
+  //navigation
   const navigate = useNavigate();
   //titre des colonnes du tableau
-const columns = [
-  { field: 'nomenclature', headerName: 'Nomenclature', width: 150, sortable: false, filterable: true, hideable: false, columnManageable: false },
-  { field: 'designation', headerName: 'Désignation', width: 150, sortable: false, filterable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
-  { field: 'existantApresEcriture', headerName: 'Existant apres écriture', width: 150, filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
-  {
-    field: 'recenser',
-    headerName: '', // Laissez le titre vide
-    width: 150,
-    filterable: false,
-    sortable: false,
-    disableColumnMenu: true,
-    hideable: false,
-    columnManageable: false,
-    renderCell: (params) => ( // Utilisez renderCell pour personnaliser la cellule
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => handleButtonClick(params.row.idRecensement)} // Remplacez handleButtonClick par votre logique de navigation
-      >
-        Recenser
-      </Button>
-    ),
-  },
-];
-const handleButtonClick = (id) => {
-  console.log(id)
-  navigate(`/recenserMateriel/${id}`);
-};
-
+  const columns = [
+    { field: 'nomenclature', headerName: 'Nomenclature',  sortable: false, filterable: true, hideable: false, columnManageable: false },
+    { field: 'designation', headerName: 'Désignation',  sortable: false, filterable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'existantApresEcriture', headerName: 'Existant apres écriture',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    {
+      field: 'recenser',
+      headerName: '',
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+      hideable: false,
+      columnManageable: false,
+      renderCell: (params) => ( 
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => RecenserMateriel(params.row.idRecensement)}
+        >
+          Recenser
+        </Button>
+      ),
+    },
+  ];
+  //recenser un matériel
+  const RecenserMateriel = (id) => {
+    navigate(`/recenserMateriel/${id}`);
+  };
+  // base url backend
   const baseUrl = 'http://localhost:8000/api';
+
+  //state à utiliser
   const [listematerielsARecenser, setListeMaterielsARecenser] = useState([]);
-  const [filteredMateriel, setFilteredMateriel] = useState([]);
+  const [materielFilter, setMaterielFilter] = useState([]);
   
 
   useEffect(() => {
     getListeMaterielsARecenser();
-    console.log(listematerielsARecenser);
   }, []);
-
+  // recuperer liste des materiels a recenser
   const getListeMaterielsARecenser = async () => {
-    const annee = 2023;
+    const now = new Date();
+    const annee = now.getFullYear();
     await axios.get(`${baseUrl}/listeMaterielARecense/${annee}`)
-      .then(res => { setListeMaterielsARecenser(res.data); setListeMaterielsARecenser(res.data);
-        setFilteredMateriel(res.data);})
-      .catch(err => console.log(err));
+    .then(res => { 
+      setListeMaterielsARecenser(res.data); 
+      setListeMaterielsARecenser(res.data);
+      setMaterielFilter(res.data);
+    })
+    .catch(err => console.log(err));
   }
+  //state pagination
   const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(30); // Set the default page size to 30
+  const [nombreLigneParPage, setNombreLigneParPage] = useState(30); 
 
-  const handlePageSizeChange = (newPageSize) => {
-    setPageSize(newPageSize);
-    setPage(1); // Reset the page to 1 when changing page size
+  const changerNombreLigneParPage = (newPageSize) => {
+    setNombreLigneParPage(newPageSize);
+    setPage(1); 
   };
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.primary.main, 1),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.primary.main, 0.75),
-  },
-}));
+  //composant pour la recherche
+  //div
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.primary.main, 1),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.primary.main, 0.75),
+    },
+  }));
+  //icone
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+  //zone de texte
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    " & .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    },
+  })); 
+  // recuperer les materiel ayant les caractere saisis dans la zone de recherche
+  const filterMateriel = (texte) => {
+    const donneesFiltrees = listematerielsARecenser.filter((row) =>
+      row.designation.toLowerCase().includes(texte.toLowerCase())
+    );
+    setMaterielFilter(donneesFiltrees);
+  };
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+  const handleSearchInputChange = (e) => {
+    filterMateriel(e.target.value);
+  };
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  },
-})); 
-// Fonction pour mettre à jour les données filtrées en fonction de la recherche
-const filterMateriel = (searchText) => {
-  const filteredData = listematerielsARecenser.filter((row) =>
-    row.designation.toLowerCase().includes(searchText.toLowerCase())
-  );
-  setFilteredMateriel(filteredData);
-  console.log(filteredData)
-};
-
-const handleSearchInputChange = (e) => {
-  filterMateriel(e.target.value);
-};
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div style={{ height: 500, width: '100%' }}>
       <Search>
-            <SearchIconWrapper>
-              <SearchIcon sx={{ color: "common.white" }} />
-            </SearchIconWrapper>
-            <StyledInputBase
+        <SearchIconWrapper>
+          <SearchIcon sx={{ color: "common.white" }} />
+        </SearchIconWrapper>
+        <StyledInputBase
           placeholder="Recherche…"
           inputProps={{ "aria-label": "recherche" }}
           sx={{ color: "common.white" }}
@@ -116,19 +124,19 @@ const handleSearchInputChange = (e) => {
               handleSearchInputChange(e)
             }
           }}
-            />
-          </Search>
-          <DataGrid
-  rows={filteredMateriel}
-  columns={columns}
-  getRowId={(row) => row.idRecensement}
-  page={page}
-  pageSize={pageSize}
-  onPageChange={(newPage) => setPage(newPage)}
-  pageSizeOptions={[30, 60, 100]}
-  onPageSizeChange={handlePageSizeChange}
-  locale="fr" // Correction ici
-/>
+        />
+      </Search>
+      <DataGrid
+        rows={materielFilter}
+        columns={columns}
+        getRowId={(row) => row.idRecensement}
+        page={page}
+        pageSize={nombreLigneParPage}
+        onPageChange={(newPage) => setPage(newPage)}
+        pageSizeOptions={[30, 60, 100]}
+        onPageSizeChange={changerNombreLigneParPage}
+        locale="fr" // Correction ici
+      />
 
     </div>
   );
--- src/components/RecenserMateriel.jsx
+++ src/components/RecenserMateriel.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {
     Dialog,
+    DialogActions,
     DialogContent,
+    DialogContentText,
     DialogTitle,
     Button, FormLabel
   } from "@mui/material";
@@ -15,174 +17,212 @@ import { useEffect } from "react";
 
 import axios from "axios";
 
-//axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-
-
-
-  //composant pour mettre le titre de la boite de dialogue
-  function BootstrapDialogTitle(props) {
-    const { children, onClose, ...other } = props;
-  
-    BootstrapDialogTitle.propTypes = {
-      children: PropTypes.node,
-      onClose: PropTypes.func.isRequired,
-    };
-    return (
-      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-        {children}
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            onClick={onClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
+//composant pour mettre le titre de la boite de dialogue du formulaire
+function BootstrapDialogTitle(props) {
+  const { children, onClose, ...other } = props;
+  BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+  };
+  return (
+    <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+      {children}
+      {onClose ? (
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={{
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: (theme) => theme.palette.grey[500],
+        }}
+      >
+      <CloseIcon />
+      </IconButton>
         ) : null}
-      </DialogTitle>
-    );
-  }
+    </DialogTitle>
+  );
+}
 
 export default function RecenserMateriel() {
-    const baseUrl = "http://localhost:8000/api";
-    // récupérer l'id du recensement a partir de l'url
-    const { id } = useParams();
-    
-    const navigate = useNavigate();
-    const [recenser, setRecenser] = useState(true);
-    const [designation, setDesignation] = useState("");
-    const [existantApresEcriture, setExistantApresEcriture] = useState("");
-    const [deficitPartArticle, setDeficitPartArticle] = useState(0);
-    const [excedentParArticle, setExcedentParArticle] = useState(0);
-    const [observation, setObservation] = useState("");
+  //url du backend
+  const baseUrl = "http://localhost:8000/api";
+  // récupérer l'id du recensement a partir de l'url
+  const { id } = useParams();
+  //navigation  
+  const navigate = useNavigate();
+  // definition des state de l'application
+  const [recenser, setRecenser] = useState(true);
+  const [designation, setDesignation] = useState("");
+  const [existantApresEcriture, setExistantApresEcriture] = useState("");
+  const [deficitPartArticle, setDeficitPartArticle] = useState(0);
+  const [excedentParArticle, setExcedentParArticle] = useState(0);
+  const [observation, setObservation] = useState("");
+  const [recensementEffectuer, setRecensementEffectuer] = useState(false);
+  const [erreur, setErreur] = useState(false);
 
-    const recenserMateriel=async(e)=>{
-        e.preventDefault();
-        console.log(deficitPartArticle)
-        console.log(excedentParArticle)
-        console.log(observation)
-        await axios.request({
-            //const response = await axios.get();
-            url: `${baseUrl}/recenserMateriel/${id}`,
-            method:"POST",
-            data: {
-                deficitParArticle: deficitPartArticle,
-                excedentParArticle: excedentParArticle,
-                observation: observation,
-            },
-            
-          })
-          .then(res=>{alert(res)})//,navigate('/liste')
-          .catch(err=>alert(err))
+  //pour effectuer le recensement dans la base de données
+  const recenserMateriel=async(e)=>{
+    e.preventDefault();
+    await axios.request({
+      url: `${baseUrl}/recenserMateriel/${id}`,
+      method:"POST",
+      data: {
+        deficitParArticle: deficitPartArticle,
+        excedentParArticle: excedentParArticle,
+        observation: observation,
+      },            
+    })
+    .then(res=>{
+      if(res.status===200){setRecensementEffectuer(true);}
+    })
+    .catch(err=>{setErreur(true)})
+  }
+  //désactiver le champ excedent s'il y a deficit
+  const remplirDeficit = (event) => {
+    const value = event.target.value;
+    setDeficitPartArticle(value);
+    if (value) {
+      setExcedentParArticle(0);
     }
-    const remplirDeficit = (event) => {
-        const value = event.target.value;
-        setDeficitPartArticle(value);
+  };
     
-        // Désactive le Field2 si Field1 a une valeur
-        if (value) {
-          setExcedentParArticle(0);
-        }
-      };
-    
-      const remplirExcedent = (event) => {
-        const value = event.target.value;
-        setExcedentParArticle(value);
-    
-        // Désactive le Field1 si Field2 a une valeur
-        if (value) {
-            setDeficitPartArticle(0);
-        }
-      };
-    const fermerFenetre=()=>{
-        setRecenser(false);
-        navigate(`/`);
+  //désactiver le champ deficit s'il y a excedent
+  const remplirExcedent = (event) => {
+    const value = event.target.value;
+    setExcedentParArticle(value);    
+    if (value) {
+      setDeficitPartArticle(0);
     }
-    useEffect(()=>{
-        const getRecensementById=async()=>{
-            const response = await axios.get(`${baseUrl}/voirRecensement/${id}`);
-            const recensement= response.data[0]
-            console.log(recensement)
-            setDesignation(recensement.designation);
-            setExistantApresEcriture(recensement.existantApresEcriture)
-        };
-        getRecensementById();
-    },[id])
+  };
+  //fermer la fenetre
+  const fermerFenetre=()=>{
+    setRecenser(false);
+    navigate(`/`);
+  }
+  useEffect(()=>{
+    //recuperer les informations du recensement pour les afficher
+    const getRecensementById=async()=>{
+      const response = await axios.get(`${baseUrl}/voirRecensement/${id}`);
+      const recensement= response.data[0]
+      setDesignation(recensement.designation);
+      setExistantApresEcriture(recensement.existantApresEcriture)
+    };
+    getRecensementById();
+  },[id])
   return (
+    <>
     <Dialog
     open={recenser}//la boite de dialogie s'ouvre quand recenser==true
     >
     <BootstrapDialogTitle id="customized-dialog-title" onClose={fermerFenetre}>
     Recenser le matériel
     </BootstrapDialogTitle>
-        <DialogContent dividers>
-            <form onSubmit={recenserMateriel} style={{ display: "flex", flexDirection: "column", gap: "10px"}}>
+    <DialogContent dividers>
+      <form onSubmit={recenserMateriel} style={{ display: "flex", flexDirection: "column", gap: "10px"}}>
         <FormLabel>Désignation</FormLabel>
         <TextField
-                id="standard-required"
-                variant="standard"
-                value={designation}
-                disabled={true}
+          id="standard-required"
+          variant="standard"
+          value={designation}
+          disabled={true}
         />
         <FormLabel>Existant après écriture</FormLabel>
         <TextField
-                id="standard-required"
-                variant="standard"
-                value={existantApresEcriture}
-                disabled={true}
+          id="standard-required"
+          variant="standard"
+          value={existantApresEcriture}
+          disabled={true}
         />
-            <FormLabel>Déficit par article</FormLabel>
+        <FormLabel>Déficit par article</FormLabel>
         <TextField
-                id="standard-required"
-                variant="standard"
-                type='number'
-                InputProps={{inputProps:{min: 0,max: existantApresEcriture} }}//max  existantApresEcriture
-                //onChange={(e) => setDeficitPartArticle(e.target.value)}
-                value={deficitPartArticle}
-                onChange={remplirDeficit}
-                disabled={excedentParArticle.length > 0}
+          id="standard-required"
+          variant="standard"
+          type='number'
+          InputProps={{inputProps:{min: 0,max: existantApresEcriture} }}//max  existantApresEcriture
+          value={deficitPartArticle}
+          onChange={remplirDeficit}
+          disabled={excedentParArticle.length > 0}
         />
         <FormLabel>Excédent par article</FormLabel>
         <TextField
-                id="standard-required"
-                variant="standard"
-                type='number'
-                InputProps={{inputProps:{min: 0,max: 10} }}
-                //onChange={(e) => setExcedentParArticle(e.target.value)}
-                value={excedentParArticle}
-                onChange={remplirExcedent}
-                disabled={deficitPartArticle.length > 0}
+          id="standard-required"
+          variant="standard"
+          type='number'
+          InputProps={{inputProps:{min: 0,max: 10} }}
+          value={excedentParArticle}
+          onChange={remplirExcedent}
+          disabled={deficitPartArticle.length > 0}
         />
         <FormLabel>Observation</FormLabel>
         <TextField
-                id="standard-required"
-                variant="standard"
-                onChange={(e) => setObservation(e.target.value)}
+          id="standard-required"
+          variant="standard"
+          onChange={(e) => setObservation(e.target.value)}
         />
-          <Button
-              variant="contained"
-              type="submit"
-              color="success"
-              sx={{ color:'black'}}
-            >
-              Recenser
-            </Button>
-            <Button
-              variant="contained"
-              type="button"
-              sx={{bgcolor:'yellow', color:'black'}}
-              onClick={fermerFenetre}
-            >
-              Annuler
-            </Button>
-            </form>
-        </DialogContent>
+        <Button
+          variant="contained"
+          type="submit"
+          color="success"
+          sx={{ color:'black'}}
+        >
+          Recenser
+        </Button>
+        <Button
+          variant="contained"
+          type="button"
+          sx={{bgcolor:'yellow', color:'black'}}
+          onClick={fermerFenetre}
+        >
+          Annuler
+        </Button>
+      </form>
+    </DialogContent>
     </Dialog>
-  )
+      {recensementEffectuer && (
+      <div>
+        <Dialog
+          open={recensementEffectuer}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Succès</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Recensement effectué avec succès
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {setRecensementEffectuer(false);navigate('/')}} autoFocus>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      )}
+      {erreur && (
+      <div>
+        <Dialog
+          open={erreur}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Erreur</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Il y a une erreur pendant le recensement du matériel
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => {setErreur(false);navigate('/')}} autoFocus>
+            OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      )}
+
+  </>
+)
 }
