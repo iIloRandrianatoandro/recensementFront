6b1f808c4066c1321c4d10d0a428f3b73bc9413b
unknown <iloorandrianatoandro.com>
2023-11-10 16:43:56 +0300

    recepitulatif affiche liste recensement par nomenclature ok

--- src/components/Recapitulatif.jsx
+++ src/components/Recapitulatif.jsx
@@ -6,6 +6,10 @@ import axios from 'axios';
 import {FormLabel} from "@mui/material";
 import TextField from '@mui/material/TextField';
 import { FormControl } from '@mui/material';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function Recapitulatif() {
     // base url backend
@@ -18,11 +22,42 @@ export default function Recapitulatif() {
     const [valeurTotaleMateriels, setValeurTotaleMateriels] = useState(0);
     const [nbArticleExcedent, setNbArticleExcedent] = useState(0);
     const [nbArticleDeficit, setNbArticleDeficit] = useState();
+    const [nomenclature, setNomenclature] = useState([]);
+    const [nomenclatureSelectionne, setNomenclatureSelectionne] = useState(3);
+    const [listeRecensementsTab, setListeRecensementsTab] = useState([]);
 
+    //state pagination
+  const [page, setPage] = useState(1);
+  const [nombreLigneParPage, setNombreLigneParPage] = useState(30); 
+  const [rows , setRows ] = useState([]);
+
+  const changerNombreLigneParPage = (newPageSize) => {
+    setNombreLigneParPage(newPageSize);
+    setPage(1); 
+  };
+  //titre des colonnes du tableau
+  const columns = [
+    { field: 'designation', headerName: 'Désignation',  sortable: false, filterable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'especeUnite', headerName: 'Espèce unite',  sortable: false, filterable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'prixUnite', headerName: 'Prix unitaire',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'existantApresEcriture', headerName: 'Existant apres écriture',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'deficitParArticle', headerName: 'Déficit par article',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'valeurDeficit', headerName: 'Valeur des déficits',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'excedentParArticle', headerName: 'Excedent apres écriture',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'valeurExcedent', headerName: 'Valeur des excédents',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'constateesParRecensement', headerName: 'Constatés par recensement',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'valeurExistant', headerName: 'Valeur des existants',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    { field: 'observation', headerName: 'Observation',  filterable: false, sortable: false, disableColumnMenu: true, hideable: false, columnManageable: false },
+    
+  ];
 
     const handleChange = (event) => {
         setAnnee(event.target.value);
     };
+    const changerNomenclature=(e)=>{
+        //console.log(e.target.value)
+        setNomenclatureSelectionne(e.target.value)
+    }
     useEffect(()=>{
         //recuperer liste des annees avec recensement
         const getAnneeExistante = async () => {
@@ -52,6 +87,11 @@ export default function Recapitulatif() {
           setValeurTotaleMateriels(res.data.valeurTotaleExistant["totalExistant"])
           setNbArticleDeficit(res.data.nbArticleAvecDeficit) 
           setNbArticleExcedent(res.data.nbArticleAvecExcedent) 
+          //console.log(res.data.nomenclatures)
+          const nomenclatures = res.data.nomenclatures.map(element => element["nomenclature"]);
+          setNomenclature(nomenclatures);
+          setListeRecensementsTab(res.data.listeRecensementsTab); 
+          setRows(res.data.recensementParNomenclature["3"]);
         })
         .catch(err => console.log(err));
       }
@@ -61,6 +101,15 @@ export default function Recapitulatif() {
         useEffect(()=>{
               getRecapitulatif()
             },[annee])
+  useEffect(()=>{
+    //console.log(nomenclatureSelectionne)
+    // recuperer les materiel ayant le nomenclature selectionnee
+    const donneesFiltrees = listeRecensementsTab.filter((row) =>
+        row.nomenclature.toLowerCase().includes(nomenclatureSelectionne)
+    );
+    //console.log(donneesFiltrees)
+    setRows(donneesFiltrees)
+  },[nomenclatureSelectionne])
   return (
     <>
       <InputLabel id="demo-simple-select-label">Annee</InputLabel>
@@ -108,7 +157,35 @@ export default function Recapitulatif() {
             value={nbArticleDeficit}
             disabled={true}
           />
-        </FormControl>
+        </FormControl>{nomenclature.length > 0 && (
+       <RadioGroup
+       row
+       aria-labelledby="demo-row-radio-buttons-group-label"
+       name="row-radio-buttons-group"
+       value={nomenclatureSelectionne}
+       onChange={changerNomenclature}
+     >
+       {nomenclature.map((nomenclatureItem) => (
+         <FormControlLabel
+           value={nomenclatureItem}
+           key={nomenclatureItem}
+           control={<Radio />}
+           label={nomenclatureItem}
+         />
+       ))}
+     </RadioGroup>
+      )}
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        getRowId={(row) => row.idRecensement}
+        page={page}
+        pageSize={nombreLigneParPage}
+        onPageChange={(newPage) => setPage(newPage)}
+        pageSizeOptions={[30, 60, 100]}
+        onPageSizeChange={changerNombreLigneParPage}
+        locale="fr" // Correction ici
+      />
     </>
   )
 }
